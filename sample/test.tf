5a20c2333cc9a0ce884629004853247d85affce6
Manohar <manu.batchu@gmail.com>
2023-03-14 11:44:20 +0530

    terraform code

--- sample/test.tf
+++ sample/test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami="ami-0a017d8ceb274537d"
   instance_type="t3.micro"
-  vpc_security_group_ids = [sg-05f4e5a12d4ecc1e7]
+  vpc_security_group_ids = ["sg-05f4e5a12d4ecc1e7"]
   tags={
     Name = "test"
   }
